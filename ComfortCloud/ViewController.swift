7fef261f3dd0a14204d0ea26ed6b49caa7248c4a
kelseylo <kelseylo326@gmail.com>
2022-08-16 14:21:35 -0400

    Initial Commit

---
+++ ComfortCloud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ComfortCloud
+//
+//  Created by Kelsey Lo on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
