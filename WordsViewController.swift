79263b079159217bf5380c5c281008f97632dcad
kelseylo <kelseylo326@gmail.com>
2022-08-17 10:26:32 -0400

    Adding additional ViewController files for each interface

---
+++ WordsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WordsViewController.swift
+//  ComfortCloud
+//
+//  Created by Kelsey Lo on 8/17/22.
+//
+
+import UIKit
+
+class WordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
